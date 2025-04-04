a563bc0e958f5a3d43c7f6d38f664cc24fd893a5
Lucas Benua <133179857+lukeb21184@users.noreply.github.com>
2025-04-03 20:21:46 -0400

    Create HelloCooking.java

---
+++ Codedex-Java-Hello_World/HelloCooking.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Step 1: Toast the buns 🍞");
+        System.out.println("Step 2: Bread your chicken 🍗"); 
+        System.out.println("Step 3: Chop the tomatoes 🍅");
+    }
+}
